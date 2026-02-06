7d942ef931c3753dd9857fe848da380531b6b56a
kienbesirius <bechjkjen@gmail.com>
2026-02-06 15:13:50 +0700

    Fix

--- src/linked_gui.py
+++ src/linked_gui.py
@@ -6,8 +6,7 @@ from src.gui import gui204_count_primes
 from src.gui import gui279_perfect_squares
 from src.gui import gui_check_fixture
 from src.gui import gui_bypass
-from src.utils.exit_guard import ExitGuard
-    
+
 def main_gui():
 	APP_NAME = "BypassSampleKinterApp"
 	root = tk.Tk(className=APP_NAME)  # <- quan trọng
@@ -18,8 +17,8 @@ def main_gui():
 
 	# app = gui204_count_primes.LeetCode204_Gui(root)
 	# app = gui279_perfect_squares.LeetCode279_Gui(root)
-	# app = gui_check_fixture.AppGUI(root)
-	app = gui_bypass.AppGUI(root)
+	app = gui_check_fixture.AppGUI(root)
+	# app = gui_bypass.AppGUI(root)
 	# app = gui.AppGUI(root)
 	root.mainloop()
 
